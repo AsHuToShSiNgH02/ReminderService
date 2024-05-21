207e91be1432ae7fb25f2bb037388aeb1b515d4e
AsHuToShSiNgH02 <ashutoshin2002@gmail.com>
2024-05-21 22:04:04 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
-const { sendBasicEmail } = require('./services/email-service');
+// const { sendBasicEmail } = require('./services/email-service');
+
+const cron = require('node-cron');
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
@@ -11,12 +13,15 @@ const setupAndStartServer = () => {
     app.listen(PORT, () => {
          console.log(`Server started at port ${PORT}`);
 
-         sendBasicEmail(
-            'support@admin.com',
-            'dummy232002@gmail.com',
-            'This is a testing email',
-            'Hey, how are you, I hope you lile the support'
-         );
+        //  sendBasicEmail(
+        //     'support@admin.com',
+        //     'dummy232002@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you, I hope you lile the support'
+        //  );
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running a task every two minutes');
+        });
     });
 }
 
